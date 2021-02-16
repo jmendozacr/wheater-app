6ce955ebcecfbf41801605c4bbdb1e0ac6ad24ee
Josue Mendoza <josuem@thehangar.cr>
2021-02-16 15:02:04 -0600

    adding new test funcion to validate onClick action

--- src/components/CityList/CityList.test.js
+++ src/components/CityList/CityList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import CityList from './CityList'; // SUT
 
 const cities = [
@@ -9,10 +9,22 @@ const cities = [
 	{ city: "Tokyo", country: "Japon" },
 ];
 
-test("CitiList renders", async () => {
+test("CityList renders", async () => {
     const { findAllByRole } = render(<CityList cities={cities}/>);
 
     const items = await findAllByRole("listitem");
 
     expect(items).toHaveLength(4);
+});
+
+test("CityList click on item", async () => {
+	const fnClickOnItem = jest.fn();
+
+	const { findAllByRole } = render(<CityList cities={cities} onClickCity={fnClickOnItem}/>);
+
+	const item = await findAllByRole("listitem");
+
+	fireEvent.click(item[0]);
+
+	expect(fnClickOnItem).toHaveBeenCalledTimes(1);
 });
