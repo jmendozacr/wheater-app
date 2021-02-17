a5ec98e59e88ccd727e1dff3185fd55bcfbc200c
Josue Mendoza <josuem@thehangar.cr>
2021-02-17 09:18:54 -0600

    adding forecastChart component

---
+++ src/components/ForecastChart/ForecastChart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+
+const ForecastChart = ({ data }) => {
+    return (
+        <div>
+            <LineChart
+                height={250}
+                width={700}
+                margin={{top: 20, bottom: 20, left: 5, right: 5}}
+                data={data}>
+                <XAxis dataKey="dayHour"/>
+                <YAxis/>
+                <CartesianGrid/>
+                <Tooltip/>
+                <Legend/>
+                <Line type="monotone" dataKey="max" stroke="#FF0000"/>
+                <Line type="monotone" dataKey="min" stroke="#0000FF"/>
+            </LineChart>
+        </div>
+    );
+}
+
+ForecastChart.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            day: PropTypes.string.isRequired,
+            min: PropTypes.number.isRequired,
+            max: PropTypes.number.isRequired,
+        })
+    )
+}
+
+export default ForecastChart;
