82f1e1623856e330352d73158f3ff92760df207c
Josue Mendoza <josuem@thehangar.cr>
2021-02-18 15:31:52 -0600

    init api integration of wheater app

--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -6,8 +7,10 @@ import PropTypes from 'prop-types';
 import CityInfo from '../CityInfo';
 import Wheater from '../Wheater';
 
-const renderCityAndCountry = eventOnClickItem => cityAndCountry => {
+const renderCityAndCountry = eventOnClickItem => (cityAndCountry, wheater) => {
   const { city, country } = cityAndCountry;
+  // console.log("wheater", wheater);
+  // const { temperature, state } = wheater;
 
   return (
     <ListItem 
@@ -27,7 +30,11 @@ const renderCityAndCountry = eventOnClickItem => cityAndCountry => {
         <Grid item
           md={3}
           xs={12}>
-          <Wheater temperature={10} state="fog"/>
+          {
+            wheater ?
+              (<Wheater temperature={wheater.temperature} state={wheater.state}/>)
+              : ("no data")
+          }
         </Grid>
       </Grid>
     </ListItem>
@@ -35,20 +42,47 @@ const renderCityAndCountry = eventOnClickItem => cityAndCountry => {
 }
 
 const CityList = ({ cities, onClickCity }) => {
+  const wheater = { temperature: 10, state: "sunny" };
+  const [allWheater, setAllWheater] = useState({});
+
+  useEffect(() => {
+    const setWheater = (city) => {
+      const appId = "dc2da2d33c522744fa9b2f5a99b74e23";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`;
+
+      axios.get(url)
+      .then(response => {
+        const { data } = response,
+          temperature = data.main.temp,
+          state= "rain";
+
+          console.log("RESPONSE", temperature);
+      })
+    }
+
+    cities.forEach(({city, country}) => {
+      setWheater(city);
+    });
+  }, [cities]);
+
+  console.log("cities", cities);
+
   return (
     <List>
       {
-        cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry))
+        cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, wheater))
       }
     </List>
   );
 }
 
 CityList.propTypes = {
-  cities: PropTypes.arrayOf(PropTypes.shape({
-    city: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired
-  })),
+  cities: PropTypes.arrayOf(
+    PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired
+    })
+  ).isRequired,
   onClickCity: PropTypes.func.isRequired,
 }
 
