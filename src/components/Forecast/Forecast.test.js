3373465313f5302af0b542272aab3636de745d76
Josue Mendoza <josuem@thehangar.cr>
2021-02-19 14:24:53 -0600

    update

--- src/components/Forecast/Forecast.test.js
+++ src/components/Forecast/Forecast.test.js
@@ -4,11 +4,11 @@ import Forecast from './Forecast';
 
 const forecastItems = [
     { hour: 12, state: "clouds", weekday: "Friday", temperature: 29 },
-    { hour: 12, state: "clear", weekday: "Monday", temperature: 9 },
-    { hour: 12, state: "rain", weekday: "Sunday", temperature: 46 },
-    { hour: 12, state: "snow", weekday: "Thursday", temperature: 3 },
-    { hour: 12, state: "drizzle", weekday: "Thursday", temperature: 21 },
-    { hour: 12, state: "thunderstorm", weekday: "Thursday", temperature: 14 },
+    { hour: 13, state: "clear", weekday: "Monday", temperature: 9 },
+    { hour: 14, state: "rain", weekday: "Sunday", temperature: 46 },
+    { hour: 15, state: "snow", weekday: "Thursday", temperature: 3 },
+    { hour: 16, state: "drizzle", weekday: "Thursday", temperature: 21 },
+    { hour: 17, state: "thunderstorm", weekday: "Thursday", temperature: 14 },
 ]
 
 test('should render Forecast', async () => {
