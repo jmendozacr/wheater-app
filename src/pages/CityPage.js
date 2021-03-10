eae5ae00bc550c58d0131071cd9efced7676c4c9
jmendozacr <mendoza077@gmail.com>
2021-03-09 21:45:55 -0600

    API integration to forecastItemList

--- src/pages/CityPage.js
+++ src/pages/CityPage.js
@@ -3,54 +3,13 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 import moment from 'moment';
+import convertUnits from 'convert-units';
 import CityInfo from './../components/CityInfo';
 import Weather from './../components/Weather';
 import WeatherDetail from './../components/WeatherDetail';
 import ForecastChart from './../components/ForecastChart';
 import ForeCast from './../components/Forecast';
 import AppFrame from './../components/AppFrame';
-
-const dataExample = [
-    {
-        "dayHour": "Jue 18",
-        "min": 14,
-        "max": 22,
-    },
-    {
-        "dayHour": "Vie 06",
-        "min": 18,
-        "max": 27,
-    },
-    {
-        "dayHour": "Vie 12",
-        "min": 18,
-        "max": 28,
-    },
-    {
-        "dayHour": "Vie 18",
-        "min": 18,
-        "max": 25,
-    },
-    {
-        "dayHour": "Sab 06",
-        "min": 15,
-        "max": 22,
-    },
-    {
-        "dayHour": "Sab 12",
-        "min": 12,
-        "max": 19,
-    }
-];
-
-const forecastItems = [
-    { hour: 12, state: "clouds", weekday: "Friday", temperature: 29 },
-    { hour: 13, state: "clear", weekday: "Monday", temperature: 9 },
-    { hour: 14, state: "rain", weekday: "Sunday", temperature: 46 },
-    { hour: 15, state: "snow", weekday: "Thursday", temperature: 3 },
-    { hour: 16, state: "drizzle", weekday: "Thursday", temperature: 21 },
-    { hour: 17, state: "thunderstorm", weekday: "Thursday", temperature: 14 },
-];
 
 const CityPage = () => {
     const { city, countryCode } = useParams();
@@ -64,26 +23,48 @@ const CityPage = () => {
        wind    = 20;
 
     useEffect(() => {
-
         const getForecast = async () => {
             const id = "dc2da2d33c522744fa9b2f5a99b74e23";
             const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${id}`;
+            const toCelsius = (temp) => Number(convertUnits(temp).from("K").to("C").toFixed(0));
+
             try {
                 const { data } = await axios.get(url);
                 const daysAhead = [0, 1, 2, 3, 4, 5];
                 const days = daysAhead.map(d => moment().add(d, "d"));
-                const dataAux = days.map(d => {
+
+                const dataAux = days.map(day => {
+                    const tempObjArray = data.list.filter(item => {
+                        const dayOfYear = moment.unix(item.dt).dayOfYear();
+
+                        return dayOfYear === day.dayOfYear();
+                    });
+
+                    const temps = tempObjArray.map(item => item.main.temp);
+                    const fallbackTemp = temps.length ? temps : [290];
+
                     return({
-                        dayHour: d.format("ddd"),
-                        min: 14,
-                        max: 22, 
+                        dayHour: day.format("ddd"),
+                        min: toCelsius(Math.min(...fallbackTemp)),
+                        max: toCelsius(Math.max(...fallbackTemp)), 
                     });
                 });
 
-                console.log("dataAux", dataAux);
-
                 setData(dataAux);
-                setForecastItemList(forecastItems);
+
+                const interval = [4, 8, 12, 16, 20, 24];
+                const forecastItemListAux = data.list
+                .filter((item, index) => interval.includes(index))
+                .map(item => {
+                    return({
+                        hour: moment.unix(item.dt).hour(),
+                        weekday: moment.unix(item.dt).format("dddd"),
+                        state: item.weather[0].main.toLowerCase(),
+                        temperature: toCelsius(item.main.temp)
+                    });
+                });
+
+                setForecastItemList(forecastItemListAux);
             } catch (error) {
                 console.log(error);
             }
