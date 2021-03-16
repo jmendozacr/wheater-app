7678916f00f3b5098414c36bc7dc120d12415bb6
jmendozacr <mendoza077@gmail.com>
2021-03-15 18:47:20 -0600

    getting humidity and wind from data

--- src/utils/transform/getAllWeather.js
+++ src/utils/transform/getAllWeather.js
@@ -4,10 +4,12 @@ import { getCityCode, toCelsius } from './../utils';
 const getAllWeather = (response, city, countryCode) => {
     const { data } = response,
         temperature = toCelsius(data.main.temp),
+        humidity = data.main.humidity,
+        wind    = data.wind.speed,
         stateFromServer = data.weather[0].main.toLowerCase(),
         state = validValues.includes(stateFromServer) ? stateFromServer : null,
         propName = getCityCode(city, countryCode), //ex: { San José-Costa Rica }
-        propValue = { temperature, state }; // ex: { temperature: 10, state: "rain" }
+        propValue = { temperature, state, humidity, wind }; // ex: { temperature: 10, state: "rain" }
 
     return ({ [propName]: propValue });
 }
